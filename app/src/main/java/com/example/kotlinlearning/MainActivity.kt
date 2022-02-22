927b6d406ed321abc26d7d3acc7c2eeb8f30b131
stevenhao <stevenhao@tencent.com>
2022-02-22 16:55:51 +0800

    Remote Config 接入
    Signed-off-by: stevenhao <stevenhao@tencent.com>

--- app/src/main/java/com/example/kotlinlearning/MainActivity.kt
+++ app/src/main/java/com/example/kotlinlearning/MainActivity.kt
@@ -4,22 +4,42 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.collection.ArrayMap
 import com.example.kotlinlearning.coroutine.CoroutineLearningActivity
 import com.example.kotlinlearning.databinding.ActivityMainBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import kotlinx.coroutines.*
 import kotlin.coroutines.EmptyCoroutineContext
 
 class MainActivity : AppCompatActivity() {
     private lateinit var loginBinding: ActivityMainBinding
+    private lateinit var remoteConfig: FirebaseRemoteConfig
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(ArrayMap<String, Any>().apply {
+            put("hahah", "123123")
+        })
+
+        remoteConfig.fetchAndActivate()
+
         loginBinding = ActivityMainBinding.inflate(layoutInflater, null, false)
         setContentView(loginBinding.root)
         loginBinding.textView.setOnClickListener {
 
             GlobalScope.launch(Dispatchers.Main) {
-                println("~~~~~" + (Thread.currentThread() == mainLooper.thread))
-                coroutineLearning()
+                val tempStr = remoteConfig.getString("hahah")
+                println("~~~~~##" + tempStr)
+//                coroutineLearning()
             }
         }
 
